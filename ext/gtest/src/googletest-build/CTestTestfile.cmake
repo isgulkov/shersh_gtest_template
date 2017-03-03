e45111519761ae7fee1fa94dd772983761b8fea0
Sammers21 <titantins@ya.ru>
2017-03-03 23:27:22 +0300

    delete gtest biaies wrond added from last commit and fix travis

--- ext/gtest/src/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sammers21/Desktop/shersh_gtest_template/ext/gtest/src/googletest
-# Build directory: /home/sammers21/Desktop/shersh_gtest_template/ext/gtest/src/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
